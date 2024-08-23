e92ce60633f241a212232aeef5c0ca94cdd5abc7
mainuldip <mainuldip@gmail.com>
2024-08-23 04:17:22 -0400

    chat-app-expo swipeable with haptic feedback docking

--- chat-app-expo/app/(tabs)/calls/index.tsx
+++ chat-app-expo/app/(tabs)/calls/index.tsx
@@ -2,21 +2,30 @@ import { View, Text, TouchableOpacity, ScrollView, FlatList, StyleSheet, Image }
 import React, { useEffect, useState } from 'react'
 import { Stack } from 'expo-router'
 import Colors from '@/constants/Colors'
-import callLog from "@/assets/data/calls.json";
+import originalCallLog from "@/assets/data/calls.json";
 import { defaultStyles } from '@/constants/Styles';
 import { Ionicons } from '@expo/vector-icons';
 import { format } from 'date-fns';
 import { SegmentedControl } from '@/components/SegmentedControl';
 import Animated, { CurvedTransition, FadeInUp, FadeOutUp } from 'react-native-reanimated';
+import SwipeableRow from '@/components/SwipeableRow';
+import * as Haptics from "expo-haptics";
+import callLog from '@/assets/data/calls.json';
 
 const transition = CurvedTransition.delay(100);
 
 const Page = () => {
+  const [callLog, setCallLog] = useState(originalCallLog);
   const [isEditing, setIsEditing] = useState(false);
   const [callsData, setCallsData] = useState(callLog)
   const [selectedOption, setSelectedOption] = useState("All")
   const onEdit = () => {
     setIsEditing(!isEditing);
+  }
+
+  const deleteRow = (itemId: string) => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    setCallLog(callLog.filter((item) => item.id !== itemId));
   }
 
   useEffect(() => {
@@ -25,7 +34,7 @@ const Page = () => {
     } else {
       setCallsData(callLog.filter((item) => item.missed === true))
     }
-  }, [selectedOption])
+  }, [selectedOption, callLog])
 
   return (
     <View style={{ flex: 1, backgroundColor: Colors.background }}>
@@ -46,27 +55,27 @@ const Page = () => {
             // itemLayoutAnimation={transition}
             ItemSeparatorComponent={() => <View style={defaultStyles.separator} />}
             renderItem={({ item, index }) => (
-              <Animated.View entering={FadeInUp.delay(index * 10)} exiting={FadeOutUp}>
-                <View style={defaultStyles.item}>
-                  <Image source={{ uri: item.img }} style={styles.avatar} />
-                  <View style={{ flex: 1, gap: 2 }}>
-                    <Text style={{ fontSize: 18, color: item.missed ? Colors.red : "#000" }}>{item.name}</Text>
-                    <View style={{ flexDirection: "row", gap: 4 }}>
-                      <Ionicons name={item.video ? "videocam" : "call"}
-                        size={16} color={Colors.gray} />
-                      <Text style={{ color: Colors.gray, flex: 1 }}>
-                        {item.incoming ? "Incoming" : "Outgoing"}
-                      </Text>
+              <SwipeableRow onDelete={() => deleteRow(item.id)}>
+                <Animated.View entering={FadeInUp.delay(index * 10)} exiting={FadeOutUp}>
+                  <View style={defaultStyles.item}>
+                    <Image source={{ uri: item.img }} style={styles.avatar} />
+                    <View style={{ flex: 1, gap: 2 }}>
+                      <Text style={{ fontSize: 18, color: item.missed ? Colors.red : "#000" }}>{item.name}</Text>
+                      <View style={{ flexDirection: "row", gap: 4 }}>
+                        <Ionicons name={item.video ? "videocam" : "call"}
+                          size={16} color={Colors.gray} />
+                        <Text style={{ color: Colors.gray, flex: 1 }}>
+                          {item.incoming ? "Incoming" : "Outgoing"}
+                        </Text>
+                      </View>
+                    </View>
+                    <View style={{ flexDirection: "row", gap: 6, alignItems: "center" }}>
+                      <Text style={{ color: Colors.gray }}>{format(item.date, "MM.dd.yy")}</Text>
+                      <Ionicons name="information-circle-outline" size={24} color={Colors.primary} />
                     </View>
                   </View>
-                  <View style={{ flexDirection: "row", gap: 6, alignItems: "center" }}>
-                    <Text style={{ color: Colors.gray }}>{format(item.date, "MM.dd.yy")}</Text>
-                    <Ionicons name="information-circle-outline" size={24} color={Colors.primary} />
-                  </View>
-                </View>
-
-              </Animated.View>
-
+                </Animated.View>
+              </SwipeableRow>
             )}
           />
         </View>
